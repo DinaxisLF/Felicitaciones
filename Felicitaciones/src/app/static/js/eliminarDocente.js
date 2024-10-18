75d5c7bee5af367fe062b5c13f6c43a51aa17527
DinaxisLF <cesar5342@hotmail.com>
2024-10-17 18:38:53 -0600

    Restructure

---
+++ Felicitaciones/src/app/static/js/eliminarDocente.js
@@ -0,0 +1,23 @@
+// Función para eliminar docente
+function eliminarDocente(id) {
+    if (confirm("¿Estás seguro de que quieres eliminar este docente?")) {
+        fetch(`/api/docentes/${id}`, {
+            method: 'DELETE', // Cambia PUT a DELETE
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            if (data.exito) {
+                alert("Docente eliminado correctamente");
+                location.reload(); // Recargar la página para reflejar los cambios
+            } else {
+                alert("No se pudo eliminar el docente");
+            }
+        })
+        .catch(error => {
+            console.error("Error al eliminar el docente:", error);
+        });
+    }
+}
